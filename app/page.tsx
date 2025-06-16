fd616fb25fcd82f994cc6611d7960702ee301b1d
ataberkakkan <akkanataberk@gmail.com>
2025-06-16 17:39:19 +0300

    Popular Companions Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,43 @@
-import { Button } from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my SaaS App</h1>
-      <Button>Let&apos;s get started</Button>
-    </div>
+    <main>
+      <h1 className="text-2xl">Popular Companions</h1>
+
+      <section className="home-section">
+        <CompanionCard
+          id={"123"}
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#E5D0FF"
+        />
+        <CompanionCard
+          id={"456"}
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="maths"
+          duration={30}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id={"789"}
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          subject="language"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList />
+        <CTA />
+      </section>
+    </main>
   );
 }
